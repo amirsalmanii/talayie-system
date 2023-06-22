e32ec34c92537a7904251de398b5608b5a18a8a0
amirsalmanii <salmaniamir1377@gmail.com>
2023-06-22 17:31:37 +0330

    user-app models created

--- talayie/app/users/models.py
+++ talayie/app/users/models.py
@@ -2,17 +2,31 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from app import db
 
 
-class User(db.Model):
+class Organization(db.Model):
+    __tablename__ = 'organizations'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    personnels = db.relationship('Personnel', backref='organization', lazy='dynamic')
+
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    personnels = db.relationship('Personnel', backref='role', lazy='dynamic')
+
+
+class Personnel(db.Model):
+    __tablename__ = 'personnels'
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(64))
+    last_name = db.Column(db.String(64))
+    date_of_birth = db.Column(db.String(128))
+    role = db.Column(db.Integer, db.ForeignKey('roles.id'))
+    organization = db.Column(db.Integer, db.ForeignKey('organizations.id'))
+
+
+class Client(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String, unique=True, nullable=False)
-    password_hash = db.Column(db.String(128))
-    
-    def __repr__(self):
-        return self.email
-    
-    def genrate_password_hash_(self, password_raw):
-        self.password_hash = generate_password_hash(password_raw)
-        
-    def check_user_password(self,password_raw):
-        return check_password_hash(self.password_hash, password_raw)
+    personnel = db.Column(db.Integer, db.ForeignKey('personnels.id'))
