0f183fc74d99af461b91e83120a610b7d356b1f2
amirsalmanii <salmaniamir1377@gmail.com>
2023-06-24 17:41:39 +0330

    add create get logic for client model

--- talayie/app/users/models.py
+++ talayie/app/users/models.py
@@ -31,3 +31,23 @@ class Client(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     personnel_id = db.Column(db.Integer, db.ForeignKey('personnels.id'))
     orders = db.relationship('Order', backref='client', lazy='dynamic')
+
+    @staticmethod
+    def get_or_create(personnel_id):
+        '''
+        always use this function when create or get object
+        example:
+        Client.get_or_create(personnel_id=3)
+        '''
+        personnel = Personnel.query.get(personnel_id)
+        if personnel:
+            client = Client.query.filter_by(personnel_id=personnel_id).first()
+            if client:
+                return client
+            else:
+                new_client = Client(personnel_id=personnel_id)
+                db.session.add(new_client)
+                db.session.commit()
+                return new_client
+        else:
+            return {"message": "he/she is not our Personnel"}
