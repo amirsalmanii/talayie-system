070aca5b2af849f0cf37c2905a0215c55c145c49
amirsalmanii <salmaniamir1377@gmail.com>
2023-06-24 19:14:34 +0330

    create client logic added

--- talayie/app/users/views.py
+++ talayie/app/users/views.py
@@ -1,7 +1,23 @@
-from flask import jsonify, request
-from . import user
+from flask_restx import Resource, fields
+from app import db, api 
+from . import user_ns
+from .models import Client
 
 
-@user.route('/test', methods=['GET'])
-def test_view():
-    return jsonify({"messeage":'test-view'})
+@user_ns.route("/create")
+class ClientCreate(Resource):
+    #serializers
+    client_serializer = api.model(
+        "Client",
+        {
+            "personnel_id": fields.Integer,
+        }
+    )
+
+    @user_ns.expect(client_serializer)
+    def post(self):
+        personnel_id = user_ns.payload.get("personnel_id")
+        client = Client.get_or_create(personnel_id)
+        if type(client) == dict: # if we have err message
+            return client
+        return {"client_id": client.id, "personnel_id": client.personnel_id}
